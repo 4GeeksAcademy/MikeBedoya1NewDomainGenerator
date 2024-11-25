bc587bf35b23502ee045e59801240ee58f3a4cae
Mike Bedoya <mikebedoya28@gmail.com>
2024-11-25 19:36:18 +0000

    domain generator without extras

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ["the", "our", "his"];
+  let adj = ["great", "super", "amazing"];
+  let noun = ["jogger", "racoon", "dog", "merchant", "driver"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        console.log(pronoun[i] + adj[b] + noun[c] + ".com");
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
